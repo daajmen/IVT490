e19a3fd743fb2d38f4e59613104af5107b10a4c4
Alexander Lundqvist <aleaxander.lundqvist@nordomatic.com>
2025-01-12 18:35:48 +0100

    fix inputtext

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from wiper import set_wiper_value
 
 while True:
-    value = int(input("Enter a value between 0 and 255: "))
+    value = int(input("Enter a value between 0 and 127: "))
     set_wiper_value(value)
     print("Wiper value set to", value)
