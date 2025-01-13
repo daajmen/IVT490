6bbde4d3bde01d1183bc7af6514eac64c6b04888
Alexander Lundqvist <aleaxander.lundqvist@nordomatic.com>
2025-01-13 19:03:43 +0100

    change names

--- serial_reader.py
+++ serial_reader.py
@@ -4,42 +4,42 @@ import serial
 ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
 
 mappings = {
-    1: ("Framledning", 0.1),
-    2: ("Ute", 0.1),
-    3: ("Tappvarmvatten Topp", 0.1),
-    4: ("Varmvatten Mitt", 0.1),
-    5: ("Värmevatten Botten", 0.1),
-    6: ("Rumstemp", 0.1),
-    7: ("Hetgas", 0.1),
-    8: ("Extra Acc-Tank", 0.1),
-    9: ("Tryckvakt", 1),
-    10: ("Högtryck", 1),
-    11: ("Lågtryck", 1),
-    12: ("Semester aktiv?", 1),
-    13: ("Kompressor aktiv", 1),
-    14: ("SV1 Öppna", 1),
-    15: ("SV1 Stäng", 1),
-    16: ("P1 Rad", 1),
-    17: ("Fläkt", 1),
-    18: ("Larm aktiv", 1),
-    19: ("Extern P2", 1),
-    20: ("LLT GT1", 0.1),
-    21: ("LL GT1", 0.1),
-    22: ("BV GT1", 0.1),
-    23: ("UL GT1", 0.1),
-    24: ("LL GT3:2", 0.1),
-    25: ("ULT GT3:2", 0.1),
-    26: ("UL GT3:2", 0.1),
-    27: ("LL GT3:3", 0.1),
-    28: ("BV GT3:3", 0.1),
-    29: ("SV3 BV Förskj", 1),
-    30: ("Effekt ink vit VV behov", 1),
-    31: ("Tillskotstimer VV behov", 1),
-    32: ("Tappv prio", 1),
-    33: ("Tillskott i %/10", 0.1),
-    34: ("Tillskott RAD", 1),
-    35: ("Tillskott Tillägg", 1),
-    36: ("Default SV2 Open", 1)
+    1: ("supply", 0.1),
+    2: ("outdoor", 0.1),
+    3: ("hotwater_top", 0.1),
+    4: ("hotwater_mid", 0.1),
+    5: ("hotwater_low", 0.1),
+    6: ("roomtemp", 0.1),
+    7: ("hotgas", 0.1),
+    8: ("extra_tank", 0.1),
+    9: ("pressure_sensor", 1),
+    10: ("high_pressure", 1),
+    11: ("low_pressure", 1),
+    12: ("vacation_mode", 1),
+    13: ("compressor", 1),
+    14: ("open_valve", 1),
+    15: ("close_valve", 1),
+    16: ("heat_pump", 1),
+    17: ("fan", 1),
+    18: ("alarm_active", 1),
+    19: ("external_pump", 1),
+    20: ("llt_gt1", 0.1),
+    21: ("ll_gt1", 0.1),
+    22: ("setpoint_gt1", 0.1),
+    23: ("ul_gt1", 0.1),
+    24: ("ll_gt3_2", 0.1),
+    25: ("ult_gt3_2", 0.1),
+    26: ("ul_gt3_2", 0.1),
+    27: ("ll_gt3_3", 0.1),
+    28: ("setpoint_gt3_3", 0.1),
+    29: ("setpoint_offset_sv3", 1),
+    30: ("power_inc_hotwater_demand", 1),
+    31: ("boost_timer_hotwater", 1),
+    32: ("hotwater_prio", 1),
+    33: ("addition", 0.1),
+    34: ("addition_rad", 1),
+    35: ("addition_supplement", 1),
+    36: ("default_sv2_open", 1)
 }
 def get_data(): 
     try:
@@ -55,7 +55,7 @@ def get_data():
                     if index < len(values):  # Kontrollera att vi inte går utanför datan
                         try:
                             raw_value = int(values[index])
-                            parsed_data[name] = raw_value * scale
+                            parsed_data[name] = round(raw_value * scale,1)
                         except ValueError:
                             print(f"Ogiltigt värde på index {index}: {values[index]}")
                             parsed_data[name] = None  # Sätt som None om det är ogiltigt
